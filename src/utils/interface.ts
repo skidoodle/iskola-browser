8aa78e9f067b6eb024c71e58845af5f511be684c
skidoodle <contact@albert.lol>
2024-02-13 08:56:29 +0100

    Add School interface for TypeScript

---
+++ src/utils/interface.ts
@@ -0,0 +1,12 @@
+/* School interface for TypeScript
+ * @param instituteCode - school code
+ * @param name - school name
+ * @param city - school city
+ * @param url - school website
+ */
+export interface School {
+  instituteCode: string
+  name: string
+  city: string
+  url: string
+}
