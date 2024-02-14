a20444f28e27058e2d5f15b4164eade7545c1627
skidoodle <contact@albert.lol>
2024-02-14 12:23:47 +0100

    Remove unnecessary comments from School interface

--- src/utils/interface.ts
+++ src/utils/interface.ts
@@ -1,9 +1,3 @@
-/* School interface for TypeScript
- * @param instituteCode - school code
- * @param name - school name
- * @param city - school city
- * @param url - school website
- */
 export interface School {
   instituteCode: string
   name: string
