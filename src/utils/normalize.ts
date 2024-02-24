d3682ba4ff96a7b8960a65345b55a0bdf1b989a8
skidoodle <contact@albert.lol>
2024-02-24 20:42:35 +0100

    Update normalize.ts

--- src/utils/normalize.ts
+++ src/utils/normalize.ts
@@ -1,7 +1,7 @@
 export const removeAccents = (str: string) => {
   return str
     .normalize('NFD')
-    .replace(/[\u0300-\u036f]/g, '') // Remove accents
-    .replace(/ /g, '_') // Replace spaces with underscores
-    .replace(/[()]/g, '') // Remove parentheses
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/ /g, '_')
+    .replace(/[()]/g, '')
 }
